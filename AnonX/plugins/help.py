1d05f54db64055242caacc7e5802c011d984a43b
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-12-02 22:43:13 +0300

    Update help.py

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -89,7 +89,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "This button is only for sudoers.", show_alert=True
+                "هذا الزر مخصص فقط للمطورين.", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
