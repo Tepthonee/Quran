0581fd82d9d4bc5f29fae45793d32002392a4832
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-12-02 22:38:43 +0300

    Update channel.py

--- AnonX/plugins/channel.py
+++ AnonX/plugins/channel.py
@@ -28,7 +28,7 @@ async def playmode_(client, message: Message, _):
     query = message.text.split(None, 2)[1].lower().strip()
     if (str(query)).lower() == "disable":
         await set_cmode(message.chat.id, None)
-        return await message.reply_text(f"ᴄʜᴀɴɴᴇʟ ᴩʟᴀʏ ᴅɪsᴀʙʟᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ʙʏ {message.from_user.first_name} ɪɴ {message.chat.title}")
+        return await message.reply_text(f"تم تعطيل تشغيل القناة بنجاح بواسطة {message.from_user.first_name} في {message.chat.title}")
     elif str(query) == "linked":
         chat = await app.get_chat(message.chat.id)
         if chat.linked_chat:
