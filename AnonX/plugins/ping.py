597136bb6f48398eff0aad480888c0c68fa49d26
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-12-03 07:41:00 +0300

    Update ping.py

--- AnonX/plugins/ping.py
+++ AnonX/plugins/ping.py
@@ -16,7 +16,7 @@ PING_COMMAND = get_command("PING_COMMAND")
 
 
 @app.on_message(
-    filters.command(PING_COMMAND)
+    filters.command(PING_COMMAND,"")
 )
 @language
 async def ping_com(client, message: Message, _):
